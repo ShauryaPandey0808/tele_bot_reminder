42f1039e31d28ee369889cae27a6c0fb0aedb71c
ShauryaPandey0808 <pandeyshaurya0808@gmail.com>
2026-03-05 08:53:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,32 +1,14 @@
+import requests
 import os
-import asyncio
-from datetime import time
-from telegram.ext import ApplicationBuilder, ContextTypes
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 CHAT_ID = os.getenv("CHAT_ID")
 
-async def morning(context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(
-        chat_id=CHAT_ID,
-        text="🌅 Good Morning Shaurya! Time to wake up!"
-    )
+message = "⏰ Reminder: Drink Water!"
 
-async def night(context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(
-        chat_id=CHAT_ID,
-        text="🌙 Sleep time! Stop coding now 😴"
-    )
+url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
 
-async def main():
-    app = ApplicationBuilder().token(BOT_TOKEN).build()
-
-    # Schedule reminders
-    app.job_queue.run_daily(morning, time(hour=7, minute=0))
-    app.job_queue.run_daily(night, time(hour=23, minute=0))
-
-    print("Reminder Bot Running 24/7...")
-    await app.run_polling()
-
-if __name__ == "__main__":
-    asyncio.run(main())
+requests.post(url, data={
+    "chat_id": CHAT_ID,
+    "text": message
+})
